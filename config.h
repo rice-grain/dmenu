2c8b5a54b5b68c03562b1680d600259ffa959fb3
EOussama <oussama.essamadi@gmail.com>
2023-12-22 16:32:26 +0100

    Added Dracula Theme Patch

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xB2;     /* amount of opacity. 0xff is opaque */
+static const unsigned int alpha = 0xBB;     /* amount of opacity. 0xff is opaque */
 static int centered = 0;		    /* -c option; centers dmenu on screen */
 static int min_width = 500;		    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -14,8 +14,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#f8f8f2", "#282a36" },
+	[SchemeSel] = { "#f8f8f2", "#6272a4" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 
